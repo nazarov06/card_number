29a92bad69570d586aca33ef7daaa7df43d8d8b2
Ozodbek Nazarov <asattorov712@gmail.com>
2023-01-25 18:03:09 +0500

    ozod

--- js/main.js
+++ js/main.js
@@ -17,12 +17,13 @@ function abs(a){
 
 function floor(n) {
   if(n% 1 >= 0.5){
-    return n.toFixed(0) - 1
+    return n.toFixed(0) 
   }
   else{
     n.toFixed(0)
   }
 }
+console.log(floor(5.2));
 
 function ceil(n) {
   if(n% 1 >= 0.5){
@@ -36,3 +37,7 @@ function ceil(n) {
 function round(n) {
   return n.toFixed(0)
 }
+function trunc(n){
+  return n*n%1
+}
+console.log(trunc(5.9));
